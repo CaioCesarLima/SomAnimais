10f799d7ebe67db706ae63c0711da78d5b664c2d
CaioCesar <cesarlimacaio@gmail.com>
2019-09-14 17:04:55 -0300

    first commit

---
+++ android/app/src/main/kotlin/caio/com/br/animais/MainActivity.kt
@@ -0,0 +1,13 @@
+package caio.com.br.animais
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
